d02b2c0e75b1061322dabb58c8b381c7f09ca49a
Ron <rohandeka614@gmail.com>
2021-06-05 14:15:59 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,44 @@
+from turtle import Screen
+from food import Food
+import time
+from snake import Snake
+from scoreboard import Scoreboard
+
+screen = Screen()
+
+screen.bgcolor("black")
+screen.setup(width=600, height=600)
+screen.title("My Snake Game!")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+is_game_on = True
+
+
+while is_game_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.track_score()
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        is_game_on = False
+        scoreboard.game_over()
+
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            is_game_on = False
+            scoreboard.game_over()
+
+screen.exitonclick()
